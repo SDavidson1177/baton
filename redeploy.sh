5d03ab925c92f0d0957588122b15810180e3efa5
SDavidson1177 <SDavidson1177@gmail.com>
2023-12-19 11:22:04 -0500

    Both relayers share configuration

--- redeploy.sh
+++ redeploy.sh
@@ -1,1 +1,1 @@
-docker compose down && ignite chain build && docker compose up -d --build chain0 chain1 chain2 relayer1
+docker compose down && ignite chain build && docker compose up -d --build chain0 chain1 chain2 relayer1 relayer2
